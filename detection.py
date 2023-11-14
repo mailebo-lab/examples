7a10e788c454bca1657c0df9d946f04894715288
Abdellah Wadjinny <34252514+awadjinn@users.noreply.github.com>
2023-11-14 20:20:44 +0100

    Update detection.py

--- detection.py
+++ detection.py
@@ -1,4 +1,4 @@
 from typing import List
 def detection(body: str, subject: str, sender: str, to: List[str], cc: List[str]):
-    condition = "annuler" in body.lower() and "commande" in body.lower()
+    condition = "annul" in body.lower() and "commande" in body.lower()
     return condition
